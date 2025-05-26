58ba6f5507100c3de9cb24769d3cd31445ef0ff3
JosuePech <josuepech015@gmail.com>
2025-05-26 10:31:08 -0500

    Conexion migration, CRUD terminado y completo

--- WebapiJosue/Program.cs
+++ WebapiJosue/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using WebapiJosue.Context;
+using WebapiJosue.Services.IServices;
+using WebapiJosue.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddTransient<IUserServices, UserServices>();
+builder.Services.AddTransient<IRolServices, RolServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
